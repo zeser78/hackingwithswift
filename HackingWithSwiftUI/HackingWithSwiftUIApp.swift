5e2385b2a687917bc25c6467643a0c3c9bd0d703
Sergio <solivares@xpaso.com>
2021-11-05 13:25:44 -0400

    Initial Commit

---
+++ HackingWithSwiftUI/HackingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUIApp.swift
+//  HackingWithSwiftUI
+//
+//  Created by Sergio Olivares on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
