40f32cd943342e53f0c886ae7965e90186bd7f01
Sergio <solivares@xpaso.com>
2021-11-06 10:55:07 -0400

    betterRest app

--- HackingWithSwiftUI/ContentView.swift
+++ HackingWithSwiftUI/ContentView.swift
@@ -4,14 +4,76 @@
 //
 //  Created by Sergio Olivares on 11/5/21.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    @State private var coffeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    @State private var rawData = Date.now
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up")
+                    .font(.headline)
+                DatePicker("please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desire time to sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffe intake")
+                    .font(.headline)
+                
+                Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cups", value: $coffeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK"){}
+                
+            } message: {
+                Text(alertMessage)
+            }
+        }
     }
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            //Change value from object to secs
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            let hour = (components.hour     ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            
+            // conveting secs to sleep time
+            let sleepTime = wakeUp - prediction.actualSleep
+            rawData = sleepTime
+            alertTitle = "Your ideal time is "
+            alertMessage = "\(sleepTime.formatted(date: .omitted, time: .shortened)) - \(rawData))"
+            
+        } catch {
+            // Something went wrong
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
+        showingAlert = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
