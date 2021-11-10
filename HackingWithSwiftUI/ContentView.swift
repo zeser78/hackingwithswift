82688f0582e0c602297d54889acfc5093a3f5d13
Sergio <solivares@xpaso.com>
2021-11-10 17:51:41 -0500

    example of animation

--- HackingWithSwiftUI/ContentView.swift
+++ HackingWithSwiftUI/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    @State private var animationRotate = 0.0
+    
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 1
+                animationRotate += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.easeOut, value: animationAmount)
+        .rotation3DEffect(.degrees(animationRotate), axis: (x: 0, y:1, z:0))
+        
+        LinearGradient(gradient: Gradient(colors:[.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200, alignment: .center)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation}
+                    .onEnded{_ in dragAmount = .zero}
+            )
+            .animation(.spring(), value: dragAmount)
+        
+        
     }
 }
 
